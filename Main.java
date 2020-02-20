205c2c412d7e02777bc9464dcfd673cd36e7f147
Dr. Kat <katychuang@users.noreply.github.com>
2020-02-20 09:58:46 -0500

    Create Main.java
    Demonstration of copy/paste in class
    
    :+1:
    😄

---
+++ Main.java
@@ -0,0 +1,11 @@
+/* Class: CISC 3130
+ * Section: TY9
+ * EmplId:
+ * Name: Mickey Mouse 
+ */
+
+public class Main {
+  public static void main(String[] args){
+    System.out.println("Hello World");
+  }
+}
